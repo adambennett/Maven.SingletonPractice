345a791177ca74eeea7306013433ad7f6a749721
Adam Bennett <nyoxidetwitter@gmail.com>
2020-03-08 00:40:25 -0500

    added another test

--- src/test/java/zipcoder/SingletonTest.java
+++ src/test/java/zipcoder/SingletonTest.java
@@ -4,9 +4,11 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import zipcoder.interfaces.Functional;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.lang.reflect.Field;
 
 public class SingletonTest {
 
@@ -27,6 +29,18 @@ public class SingletonTest {
     public void tearDown() {
         System.setOut(originalOut);
         System.setErr(originalErr);
+    }
+
+    @Test
+    public void getFields() {
+        try {
+            Class clazz = Singleton.class.getClass();
+            Field function = Singleton.class.getDeclaredField("functional");
+            function.setAccessible(true);
+            Functional ref = (Functional) function.get("");
+            ref.p("Adam's Singleton Lab");
+            Assert.assertEquals("Adam's Singleton Lab", outContent.toString().trim());
+        } catch (NoSuchFieldException | IllegalAccessException ex) { Assert.assertTrue(false); }
     }
 
     @Test
